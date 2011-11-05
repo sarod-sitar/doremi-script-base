59c5b05b0196c1355d2361c8842db330f6183b8f
John Rothfield <rthfield@sonic.net>
2011-11-05 15:29:18 -0700

    cleanup

--- src/js/sargam2ly.js
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env node
-fs=require('fs');
-require('./sargam_parser.js')
-parser=SargamParser
-x=parser.parse(fs.readFileSync('/dev/stdin').toString())
-process.stdout.write(sys.inspect(x,false,null))
