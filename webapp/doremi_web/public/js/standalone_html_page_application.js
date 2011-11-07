d13d1a39bd6a8dea9a56048ac755bf5bc020686c
John Rothfield <rthfield@sonic.net>
2011-11-07 12:46:01 -0800

    added bin script to generate html page

--- webapp/doremi_web/public/js/standalone_html_page_application.js
+++ webapp/doremi_web/public/js/standalone_html_page_application.js
@@ -3,9 +3,7 @@
   $ = jQuery;
   root = typeof exports !== "undefined" && exports !== null ? exports : this;
   $(document).ready(function() {
-    var renderer;
     window.debug = false;
-    renderer = new SargamHtmlRenderer;
-    return renderer.adjust_slurs_in_dom();
+    return adjust_slurs_in_dom();
   });
 }).call(this);
