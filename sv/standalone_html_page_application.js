c99a75e02734f35237a14512ba96ae5f89b62e55
John Rothfield <rthfield@sonic.net>
2011-12-07 13:51:39 -0800

    working on routing. changed hrefs and src attributes to be related to root of site rather than current document (means putting / at start of each).

--- sv/standalone_html_page_application.js
+++
@@ -1,8 +0,0 @@
-(function() {
-  var root;
-  root = typeof exports !== "undefined" && exports !== null ? exports : this;
-  $(document).ready(function() {
-    window.debug = false;
-    return adjust_slurs_in_dom();
-  });
-}).call(this);
